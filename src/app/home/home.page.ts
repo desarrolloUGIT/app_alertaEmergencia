fca2c298097f1a85673c6b130b2907f21f7477b6
mauricio.donoso@brechadigital.cl <samuel@MacBook-Pro-de-Samuel.local>
2022-06-24 11:40:36 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import IdentifyParameters from "@arcgis/core/rest/support/IdentifyParameters";
+import * as identify from "@arcgis/core/rest/identify";
+import MapImageLayer from "@arcgis/core/layers/MapImageLayer";
+import Map from '@arcgis/core/Map';
+import MapView from '@arcgis/core/views/MapView';
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  latitude: any = 0; //latitude
+  longitude: any = 0; //longitude
+  view;
+  map;
+  mapImageLayerEmergencia: any;
+  currentQuery : string;
+  coordenadas: any;
+  basemap = 'streets-vector'
   constructor() {}
+
+  ngOnInit(): void {
+    this.initailize()
+  }
+
+  async initailize(){
+    this.map = new Map({
+      basemap: this.basemap
+    });
+    const vialidadRedVialURL = 'https://rest-sit.mop.gob.cl/arcgis/rest/services/VIALIDAD/Red_Vial_Chile/MapServer';
+    this.mapImageLayerEmergencia = new MapImageLayer({
+      url: vialidadRedVialURL
+    })
+    this.view = new MapView({
+      container: "container", 
+      map: this.map, 
+      constraints : {
+        minZoom :2,
+        maxZoom:21
+      },
+    });
+    this.view.center = [-70.673676, -33.447487]
+    this.view.zoom = 10;  
+    this.map.add(this.mapImageLayerEmergencia);
+
+    await this.view.when(() => {
+      this.view.on("click",(e)=>{
+        // this.mapViewIdentify(e.mapPoint,IdentifyTask,IdentifyParameters,vialidadRedVialURL);
+      });
+    });
+  }
 
 }
