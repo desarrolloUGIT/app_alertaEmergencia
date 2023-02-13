1bd2591478dbf70b483bee763c7c7ebb9e02f37c
MauricioDonoso <38051881+MauricioDonoso@users.noreply.github.com>
2023-02-13 16:09:05 -0300

    version arriba febrero2023 enviada a vialidad

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { addIcons } from 'ionicons';
+import { App } from '@capacitor/app';
+
 addIcons({
   'not-signal': 'assets/img/not_signal.svg',
 });
@@ -9,10 +11,14 @@ addIcons({
   styleUrls: ['./help.page.scss'],
 })
 export class HelpPage implements OnInit {
-
+  version;
   constructor() { }
 
   ngOnInit() {
+    App.getInfo().then(e=>{
+      this.version = e.version;
+      console.log('INFOOOOOOOOOO =>',e)
+    })
   }
 
 }
